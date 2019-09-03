1b49b46fefaf1bdb73c073b3fd3972140757d4f9
simplejason <simplejason.coder@gmail.com>
2019-09-03 16:15:21 +0800

    feat(button): add button demo

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.less'],
+  selector       : 'app-button',
+  templateUrl    : './button.component.html',
+  styleUrls      : [ './button.component.less' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  clickEvent() {
+    console.log('You clicked me!');
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
