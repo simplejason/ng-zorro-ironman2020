26f2baeac2544f04c64ec2262e0b4266ac402155
simplejason <simplejason.coder@gmail.com>
2019-09-07 14:36:05 +0800

    fix(todo): move list to single folder

--- src/app/components/demos/todo/todo.component.ts
+++ src/app/components/demos/todo/todo.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ITask } from 'interfaces';
 import differenceInCalendarDays from 'date-fns/differenceInCalendarDays';
-import { NzDrawerRef, NzDrawerService, NzMessageService } from 'ng-zorro-antd';
-import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { ITask } from 'interfaces';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector       : 'app-todo',
@@ -14,14 +13,6 @@ import { TaskDetailComponent } from './task-detail/task-detail.component';
 export class TodoComponent implements OnInit {
   showAddForm = false;
   createForm: FormGroup;
-  activatedTask: ITask;
-  drawerRef: NzDrawerRef;
-
-  tableWidthConfig = [
-    '30px',
-    '600px',
-    '50px'
-  ];
   today = new Date();
   listOfTodoTasks: ITask[] = [
     {
@@ -47,15 +38,6 @@ export class TodoComponent implements OnInit {
     // Can not select days before today and today
     return differenceInCalendarDays(current, this.today) < 0;
   };
-
-  /**
-   * Check the task
-   */
-  checkTask(task: ITask): void {
-    this.listOfTodoTasks = this.listOfTodoTasks.filter(v => v.id !== task.id);
-    const successText = this.listOfTodoTasks.length ? '恭喜完成一個任務，繼續加油！' : '恭喜你完成了所有待辦任務！';
-    this.nzMessageService.success(successText);
-  }
 
   /**
    * add task
@@ -91,53 +73,10 @@ export class TodoComponent implements OnInit {
     this.showAddForm = isShow;
   }
 
-  /**
-   * active task to edit or delete
-   */
-  setActivatedTask(task: ITask) {
-    this.activatedTask = task;
-  }
-
-  showEditTask() {
-    // hide add form
-    this.showTaskForm(false);
-
-    this.drawerRef = this.nzDrawerService.create({
-      nzTitle        : this.activatedTask.name,
-      nzContent      : TaskDetailComponent,
-      nzWidth        : 400,
-      nzContentParams: {
-        task: this.activatedTask
-      }
-    });
-
-    this.drawerRef.afterClose.subscribe(task => {
-      if (task) {
-        this.listOfTodoTasks = this.listOfTodoTasks.map(v => {
-          if (v.id === task.id) {
-            v = task;
-          }
-          return v;
-        });
-        this.cdr.markForCheck();
-        this.nzMessageService.success('成功更新了這個任務！');
-
-      }
-    });
-  }
-
-  /**
-   * delete a task
-   */
-  deleteTask() {
-    this.listOfTodoTasks = this.listOfTodoTasks.filter(v => v.id !== this.activatedTask.id);
-  }
-
   constructor(
     private fb: FormBuilder,
     private cdr: ChangeDetectorRef,
-    private nzMessageService: NzMessageService,
-    private nzDrawerService: NzDrawerService
+    private nzMessageService: NzMessageService
   ) {
     this.createForm = this.fb.group({
       name       : [ null, [ Validators.required ] ],
