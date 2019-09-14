c8fb3513235412e60d2ca9cf06884728759112ee
simplejason <simplejason.coder@gmail.com>
2019-09-14 21:15:12 +0800

    fix(hacker-news): add nz-list

--- src/app/components/demos/hacker-news/hacker-news.component.ts
+++ src/app/components/demos/hacker-news/hacker-news.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { IBaseStoryType } from 'interfaces';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Hit, IBaseStoryType, IStory } from 'interfaces';
 import { HackerNewsService } from '../../../services/hacker-news.service';
 
 @Component({
@@ -12,20 +12,34 @@ export class HackerNewsComponent implements OnInit {
 
   currentPage = 1;
   pageSize = 20;
+  loading = true;
+  listOfNews: Hit[] = [];
+
+  getStories() {
+    this.hackerNewsService.getStoriesByAlgolia().subscribe(data => {
+      this.listOfNews = data.hits;
+      this.loading = false;
+      this.cdr.markForCheck();
+      console.log(this.listOfNews);
+    }, (err) => {
+      console.error(err);
+      this.loading = false;
+      this.cdr.markForCheck();
+    });
+  }
 
   constructor(
-    private hackerNewsService: HackerNewsService
+    private hackerNewsService: HackerNewsService,
+    private cdr: ChangeDetectorRef
   ) {
   }
 
   ngOnInit() {
-    this.hackerNewsService.getStories(this.currentPage, this.pageSize, IBaseStoryType.BEST).subscribe(data => {
-      console.log(data, 'offical data');
-    });
-
-    this.hackerNewsService.getStoriesByAlgolia().subscribe(data => {
-      console.log(data, 'algolia data');
-    });
+    // this.hackerNewsService.getStories(this.currentPage, this.pageSize, IBaseStoryType.BEST).subscribe(data => {
+    //   console.log(data, 'offical data');
+    // });
+    //
+    this.getStories();
   }
 
 }
